31e1460ce1781d1883b23c8036041dfd2508eb5d
Da-Mell <Damellfaines8@gmail.com>
2021-03-13 14:22:58 -0500

    commit tree 3e9cdd0b954b35abecf603831a7a22d577678873

--- index.js
+++ index.js
@@ -11,3 +11,17 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+const greet = (str) => {
+  // If the time is earlier than 12pm, return "Good Morning".
+  // If the time is between 12pm and 5pm, return "Good Afternoon".
+  //   If the time is later than 5pm, return "Good Evening".
+  let nowTime = str.split(':');
+  let nowHour = parseInt(nowTime[0]);
+  if (nowHour < "12pm") {
+    return "Good Morning";
+  } else if (nowHour > "5pm") {
+    return "Good Evening";
+  } else {
+    return "Good Afternoon";
+  }
+}
